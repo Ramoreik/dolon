b0bc727412f3163cf0d892fa2dcf03e620daf1ed
det <deft@pop-os.localdomain>
2021-01-06 21:03:42 -0500

    succeeded shell

--- src/main.rs
+++ src/main.rs
@@ -21,26 +21,26 @@ fn shell() {
                 std::process::exit(0);
             }
             _ => {
-                let output = command(cmd.to_lowercase());
-                // println!("{}", String::from_utf8(output.stdout).unwrap());
-                println!("{:?}", output.stdout);
+                let output: String = String::from_utf8(
+                                    command(cmd.to_lowercase()).stdout).unwrap();
+                println!("=========================");
+                println!("{}", output);
             },
         }
     }
 }
 
 fn command(user_cmd: String) -> Output {
-    let mut cmd: Vec<&str> = user_cmd.split("").collect();
     let output = if cfg!(target_os = "windows") {
-        cmd.insert(0, "/C");
         Command::new("C:\\Windows\\System32\\cmd.exe")
-                .args(&cmd)
+                .arg("/C")
+                .arg(user_cmd)
                 .output()
                 .expect("failed to execute process")
     } else {
         Command::new("sh")
                 .arg("-c")
-                .arg("")
+                .arg(user_cmd)
                 .output()
                 .expect("failed to execute process")
     };
