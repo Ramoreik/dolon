1850eb1e6116c78fbe5f38e677d303e7a1937909
det <deft@pop-os.localdomain>
2021-01-06 20:17:20 -0500

    implemented basic loop + input handler

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,27 @@
-use std::fs;
+use std::process::Command;
+use std::error::Error;
+use read_input::prelude::*;
 
-fn main() {
-    println!("Hello, world!");
+static USAGE: &str = "im the help message yo.";
+
+
+fn main() -> Result<(), Box<dyn Error>>{
+    let username: String = input::<String>().msg("Enter your username: ").get();
+    println!("Hello mr.{} . :)", username);
+    shell();
+    Ok(())
+}
+
+fn shell(){
+    loop {
+        let command: String = input::<String>().msg("[?] Enter your command: ").get();
+        match command.to_lowercase().as_str(){
+            "help" => println!("{}", USAGE),
+            "quit"|"exit" => {
+                println!("[!] Exit requested, terminating.");
+                std::process::exit(0);
+            },
+            _ => eprintln!("[!] Command not found."),
+        }
+    }
 }
---
+++ todo.md
@@ -0,0 +1,10 @@
+# TODO
+---
+
+### Read User Input [OK]
+
+### Have a match loop to handle input [OK]
+
+### Let the desired shell handle other user commands
+
+### Have basic commands for the user
